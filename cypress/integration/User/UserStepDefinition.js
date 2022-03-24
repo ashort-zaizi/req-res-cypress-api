9ee71e8a889c2730938b8cd2c415ab6596aa123c
Andy Short <andyshortzaizi.com@Andys-MBP.home>
2022-03-24 21:38:24 +0000

    Updated User step definition to include assertions

--- cypress/integration/User/UserStepDefinition.js
+++ cypress/integration/User/UserStepDefinition.js
@@ -24,7 +24,9 @@ Then('response status code equals 200', () => {
 })
 
 And('response body contains all users', () => {
-    cy.log(response)
-    // expect(response.body.name).to.eq("Andy Short");
+    cy.log(response.body.data[1].email)
+    // Expect the respoinse body to contain more than 5 users
+    cy.log(response.body.data.length)
+    expect(response.body.data.length).to.be.greaterThan(1);
 
 })
